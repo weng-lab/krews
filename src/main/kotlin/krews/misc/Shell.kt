79f42da9e63b94c4df74dfb9169fe8753e7d542c
Jason Brooks <jasonbrooks85@gmail.com>
2019-04-02 16:08:06 -0400

    Increased log level of slurm command executor

--- src/main/kotlin/krews/misc/Shell.kt
+++ src/main/kotlin/krews/misc/Shell.kt
@@ -11,13 +11,13 @@ private val log = KotlinLogging.logger {}
 class CommandExecutor(private val sshConfig: SshConfig?) {
 
     fun exec(command: String): String {
-        log.debug { "Executing command:\n$command" }
+        log.info { "Executing command:\n$command" }
 
         val runCmd = if (sshConfig != null) "ssh ${sshConfig.user}@${sshConfig.host} -p ${sshConfig.port} -X $command"
             else command
         val process = Runtime.getRuntime().exec(runCmd)
         val result = process.inputStream.reader().readText()
-        log.debug { "Command results:\n$result" }
+        log.info { "Command results:\n$result" }
 
         val error = process.errorStream.reader().readText()
         if (error.isNotBlank()) throw Exception("Encountered error during command execution: $error")
