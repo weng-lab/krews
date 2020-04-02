43b7d830fcb363c0bd50a6a72e934477dc88fde0
Jason Brooks <jasonbrooks85@gmail.com>
2020-04-02 10:57:08 -0400

    Updated gcp api storage lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "io.krews"
-version = "0.10.3"
+version = "0.10.4"
 
 repositories {
     maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
@@ -33,7 +33,7 @@ dependencies {
     compile("org.flywaydb", "flyway-core", "5.2.0")
     compile("com.zaxxer", "HikariCP", "3.1.0")
     compile("com.google.apis", "google-api-services-lifesciences", "v2beta-rev5-1.25.0")
-    compile("com.google.apis", "google-api-services-storage", "v1-rev20180905-1.26.0")
+    compile("com.google.apis", "google-api-services-storage", "v1-rev171-1.25.0")
     compile("com.github.ajalt","clikt", "1.5.0")
     compile( "org.jetbrains.kotlinx", "kotlinx-html-jvm", "0.6.11")
     compile( "org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.1.1")
